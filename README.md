539d6067b78ee299364d19bd06535c7bdb788535
Gevrai Jodoin-Tremblay <gevrai@gmail.com>
2015-10-16 01:37:17 -0400

    Number multiplication

--- README.md
+++ README.md
@@ -2,5 +2,58 @@
 
 Memory Verification: Valgrind
 
-TODO Redo print numbers not recursively.
-TODO number->length is int enough for insanely long numbers ?
+- Redo print numbers not recursively.
+- malloc errors
+- 999 algorithm pour addition de nombres négatifs?
+- Trimer les zéros inutiles au début d'un nombre
+- Vérifier que les nombres ne soient pas vide avant opérations
+
+
+
+Algorithme d'addition:
+
+L'algorithme d'addition s'inspire fortement de la méthode classique sur papier apprise à l'école primaire, avec quelques cas à traiter dans le cas d'addition de nombres négatifs. En effet, l'algorithme calcule le résultat caractère par caractère, en additionnant les deux chaînes, de la figure la moins significative vers la plus significative, sans oublier les retenus s'il y a lieu.
+
+Par exemple, prenons les chaînes A= "a_n, a_n-1, ..., a_k, ..., a_1" de longueur n et B= "b_m, b_m-1, ..., b_k, b_1" de longueur m.
+Dans le cas où A et B sont les deux positifs, pour additionner les deux chaînes, il faudra itérativement effectuer c_k-1 + a_k + b_k = c_k r_k pour créer une nouvelle chaîne R= "r_o, r_o-1, ..., r_k, ..., r_1" de longueur o = max{m,n} ou max{m,n}+1 dépandant de la dernière retenue. IL est facile de traiter le cas où A et B ne sont pas de même tailles, simplement en "ajoutant" des zéros au début du chiffre. Simplement, on peut dire que si k > n, alors a_n = 0, et le même principe pour B.
+
+Dans le cas où les deux chaînes sont négatives, on peut simplement additionner celles-ci comme si elles étaients positives et assumer que la réponse sera la négation du résultat. Ceci est du à la distributivité de l'addition tel que (-a + -b) = -(a+b).
+
+Ne reste maintenant que le cas où les chaînes sont de signes opposés
+
+
+Algorithme de multiplication:
+
+mult(Number n1, n2)
+
+	où cellule -> {int valeur; cellule prochain}
+	cellule c1_curr = n1.debut,
+			c2_curr = n2.debut,
+			cr_curr;
+
+	où Number -> {booléen estPositif; cellule début}
+	Number résultat;
+	résultat.estPositif = vrai si n1 et n2 sont de même signe, faux sinon;
+
+	int temp;
+
+	int i = 0,
+		j;
+	tant que (c1_curr existe){
+		j = 0;
+		tant que (c2_curr existe){
+			cr_curr = (i+j)ème cellule de "résultat" en créant les cellules qui n'existe pas au passage
+			cr_curr += c1_curr.valeur * c2_curr.valeur;
+			tant que (cr_curr.valeur > 9){
+				si (cr_curr.prochain n'existe pas) 
+					créer la cellule cr_curr.prochain avec valeur 0
+				cr_curr.prochain.valeur += cr_curr.valeur / 10 (division entière)
+				cr_curr.valeur = cr_curr.valeur % 10;
+				cr_curr = cr_curr.prochain
+			}
+			j++
+		}
+		i++
+	}
+
+	retourne number;
--- number.c
+++ number.c
@@ -9,9 +9,15 @@ struct cell {
 
 typedef struct {
 	int isPositive;
-	int length;
 	struct cell *first_p;
 } Number;
+
+struct cell* createCell(int figure){
+	struct cell *newCell = malloc(sizeof(struct cell));
+	newCell->figure = figure;
+	newCell->next_p = NULL;
+	return newCell;
+}
 
 void deleteNumber(Number* number){
 	struct cell *tempCell,
@@ -46,50 +52,48 @@ void printNumber(Number* number){
 }
 
 Number* addNumbers(Number *n1, Number *n2){
-	// This function needs that the first number has more or the same ammount of numeral than the second
-	// If this is not the case we need to swap them
-	struct cell *c1_current, *c2_current;
-	c1_current = n1->first_p;
-	c2_current = n2->first_p;
-	while(c2_current != NULL){
-		if (c1_current == NULL) {
-			return addNumbers(n2,n1);
-		}
-		c1_current = c1_current->next_p;
-		c2_current = c2_current->next_p;
- 	}
-
 	// Init the new number (result of the sum)
 	Number *result = malloc(sizeof(Number));
 	result->first_p = NULL;
-	result->length = 0;
 
-	//Necessary variables for calculations (as well as c1/c2_current already created)
-	struct cell *cresult_current, *ctemp;
+	//Necessary variables for calculations
+	int carry = 0,
+		tempValue,
+		n2_isPositive;
+	int c1_done = 0, c2_done = 0;
+	struct cell *c1_current, 
+				*c2_current,
+				*cresult_current,
+				*ctemp;
 	struct cell czero;
 	czero.figure = 0;
 	czero.next_p = &czero;
 
-	int carry = 0, tempValue, lastc2Value;
-	//Case 1: +/+
-	if (n1-> isPositive && n2->isPositive){
-		result->isPositive = 1;
+	//Case 1: +/+ or -/-
+	if (n1-> isPositive == n2->isPositive){
+		// Since (-n1+-n2) = -(n1+n2), the result takes either n1 or n2's sign
+		result->isPositive = n1->isPositive;
+
 		c1_current = n1->first_p;
 		c2_current = n2->first_p;
-		while (c1_current != NULL){
+		while ((!c1_done) || (!c2_done)){
 			// Init new cell
-			ctemp = malloc(sizeof(struct cell));
-			ctemp->next_p = NULL;
+			ctemp = createCell(0);
+
 			//DEGUEU: initialise numbers head figure
 			if (result->first_p == NULL)
 				result->first_p = ctemp;
 			else
 				cresult_current->next_p = ctemp;
+
 			//Addition with carry
 			tempValue = carry + (c1_current->figure) + (c2_current->figure);
 			if (tempValue > 9){
 				carry = 1;
-				ctemp->figure = tempValue%10;
+				ctemp->figure = tempValue - 10;
+			} else if (tempValue < 0){
+				carry = -1;
+				ctemp->figure = tempValue + 10;
 			} else {
 				carry = 0;
 				ctemp->figure = tempValue;
@@ -97,66 +101,113 @@ Number* addNumbers(Number *n1, Number *n2){
 
 			//Set up next iteration
 			cresult_current = ctemp;
-			c1_current = c1_current->next_p;
-			// Is n2 done?
-			if ((c2_current->next_p) != NULL){
+			// Does n1 and n2 have more digit?
+			// If so, introduce dummy cell (0) to continue calculations
+			if((c1_current->next_p) != NULL) 
+				c1_current = c1_current->next_p;
+			else {
+				c1_current = &czero;
+				c1_done = 1;
+			}
+			if ((c2_current->next_p) != NULL) 
 				c2_current = c2_current->next_p;
-			} 
-			// Introduce dummy cell to continue calculations
 			else {
 				c2_current = &czero;
+				c2_done = 1;
 			}
-
-			(result->length)++;
 		}
+
 		if (carry){
-			ctemp = malloc(sizeof(struct cell));
+			ctemp = createCell(carry);
 			cresult_current->next_p = ctemp;
-			ctemp->next_p = NULL;
-			ctemp->figure = carry;
-			(result->length)++;
 		}
 	}
 
 	return result;
 }
 
+Number* multiplyNumbers(Number *n1, Number *n2){
+
+	Number *result = malloc(sizeof(Number));
+	result->first_p = createCell(0);
+	// Same sign means result is positive, otherwise it's negative.
+	result->isPositive = (n1->isPositive == n2->isPositive) ? 1 : 0;
+
+	struct cell *c1_current, *c2_current, *cr_current, *ctemp;
+	int i,j,k;
+
+	c1_current = n1->first_p;
+	i = 0;
+	while(c1_current != NULL){
+		c2_current = n2->first_p;
+		j = 0;
+		while(c2_current != NULL){
+			cr_current = result->first_p;
+			// Move to the correct cell while creating them if need be
+			for(k = 0; k < i+j; k++){
+				ctemp = cr_current->next_p;
+				if(ctemp == NULL){
+					ctemp = createCell(0);
+					cr_current->next_p = ctemp;
+				}
+				cr_current = ctemp;
+			}
+			// Cell multiplication (mitosis?)
+			cr_current->figure += (c1_current->figure) * (c2_current->figure);
+			// Take care of carry
+			while(cr_current->figure > 9){
+				ctemp = cr_current->next_p;
+				if (ctemp == NULL){
+					ctemp = createCell(0);
+					cr_current->next_p = ctemp;
+				}
+				ctemp->figure += cr_current->figure / 10;// ctemp <=> cr_current->next_p
+				cr_current->figure %= 10;
+				cr_current = ctemp;
+			}
+			c2_current = c2_current->next_p;
+			j++;
+		}
+		c1_current = c1_current->next_p;
+		i++;
+	}
+	return result;
+}
+
 // TODO check if the reading is really an integer
 Number* createNumberFromWordCommandLine(int isPositive, char c){
 	// Create first cell
-	struct cell *currentCell_p = malloc(sizeof(struct cell));
-	if (currentCell_p == NULL);// ERROR -----------------------------------------------------------------------------------MEMORY ERROR
-	currentCell_p->figure = charToInt(c);
-	currentCell_p->next_p = NULL;
+	struct cell *currentCell_p = createCell(charToInt(c));
 
-	int length = 1;
 	//Continue reading line until invalid character
 	while ((c = getchar()) >= '0' && c <= '9'){
 		struct cell *newCell_p = malloc(sizeof(struct cell));
 		newCell_p->figure = charToInt(c);
 		newCell_p->next_p = currentCell_p;
 		currentCell_p = newCell_p;
-		length++;
 	}
 
 	// Set up Number and return
 	Number *number = malloc(sizeof(Number));
 	if (number == NULL);	// --------------------------------------------------------------------------------------------MEMORY ERROR
 	number->isPositive = isPositive;
-	number->length = length;
 	number->first_p = currentCell_p;
 	return number;
 }
 
+
+// Don't forget to comment this once UNIT TESTING is done.
 int main(){
 	Number *n1 = createNumberFromWordCommandLine(1, getchar());
 	Number *n2 = createNumberFromWordCommandLine(1, getchar());
-	Number *n3 = addNumbers(n1,n2);
+	Number *n3 = multiplyNumbers(n1,n2);
+
 	printNumber(n1);
-	printf("+ ");
+	printf("* ");
 	printNumber(n2);
 	printf("= ");
 	printNumber(n3);
+
 	deleteNumber(n1);
 	deleteNumber(n2);
 	deleteNumber(n3);
